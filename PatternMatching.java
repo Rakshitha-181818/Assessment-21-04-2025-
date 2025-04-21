aa6f1a721b9d38fd5391b6ee6f2a7c60c37b5517
Rakshitha-181818 <rakshitharr49@gmail.com>
2025-04-21 18:56:53 +0530

    Add files via upload

---
+++ PatternMatching.java
@@ -0,0 +1,28 @@
+package pattern_matching;
+
+import java.util.Scanner;
+public class PatternMatching {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		        int rows = 5;
+
+		        for (int i = 1; i <= rows; i++) {
+		            
+		            for (int j = i; j < rows; j++) {
+		                System.out.print(" ");
+		            }
+		          
+		          
+		                System.out.print("*");
+		            }
+		            System.out.println(); 
+		        }
+		    }
+		
+
+	
+	
+
+
